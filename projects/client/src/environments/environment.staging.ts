a18b18a0b568c3c2d278beff189379e32cbd105e
rodrigue <rodriguet802@gmail.com>
2025-08-05 03:14:55 +0200

    feat: add staging environment configuration and file replacements in angular.json

---
+++ projects/client/src/environments/environment.staging.ts
@@ -0,0 +1,15 @@
+// environment.staging.ts
+export const environment = {
+  production: false,
+  api: {
+    ip: 'stg.soncollab.com',
+    protocol: 'https',
+    fullUrl: 'https://stg.soncollab.com/api',
+    baseUrl: 'https://stg.soncollab.com'
+  },
+
+  app: {
+    name: 'Soncollab',
+    version: '1.0.0',
+  },
+};
