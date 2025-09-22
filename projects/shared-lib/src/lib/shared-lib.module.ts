7d2e6a0b5a3eab644ea701f756810336cefde91a
togberodrigue <soncollabapp@gmai.com>
2025-09-22 03:36:53 +0200

    Integrates shared-lib build and copy into the Dockerfile.

--- projects/shared-lib/src/lib/shared-lib.module.ts
+++ projects/shared-lib/src/lib/shared-lib.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import {ChoicesModule} from './modules/choices/choices.module';
 import {ToastModule} from './modules/toast/toast.module';
+import {LanguageOrchestratorService, LanguageService, RecaptchaService, ThemeService} from './services';
+import {ToastService} from './modules/toast';
 @NgModule({
   imports: [
     ChoicesModule,
     ToastModule
+  ],
+  providers: [
+    LanguageService,
+    LanguageOrchestratorService,
+    ThemeService,
+    RecaptchaService,
+    ToastService
   ],
   exports: [
     ChoicesModule,
