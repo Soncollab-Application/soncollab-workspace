a6864aa0952d276ef93cf5062a95fc1392ec9c02
Aklunonhou Rodrigue TOGBE <soncollabapp@gmail.com>
2025-10-03 03:01:49 +0200

    Exposes new alert component and its types in shared-lib.

--- projects/back-office/src/app/pages/admin/admin-dashboard/admin-dashboard.ts
+++ projects/back-office/src/app/pages/admin/admin-dashboard/admin-dashboard.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin-dashboard',
   imports: [
+
   ],
   templateUrl: './admin-dashboard.html',
   styleUrl: './admin-dashboard.css'
