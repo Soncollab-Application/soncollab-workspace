923543d0b038a789fedcd575191ac0609c553285
Aklunonhou Rodrigue TOGBE <soncollabapp@gmail.com>
2025-10-03 02:42:29 +0200

    Replaces NgbPopover with shared-lib tooltip for theme toggle.

--- projects/back-office/src/app/pages/admin/admin-dashboard/admin-dashboard.ts
+++ projects/back-office/src/app/pages/admin/admin-dashboard/admin-dashboard.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-admin-dashboard',
   imports: [
-
   ],
   templateUrl: './admin-dashboard.html',
   styleUrl: './admin-dashboard.css'
